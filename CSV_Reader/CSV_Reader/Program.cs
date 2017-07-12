18f10fb7758f1017abf0183fa3fdac850fb2c543
Micah Netz <mnetz@2ndwatch.com>
2017-07-12 14:09:45 -0700

    Added solution and base class

---
+++ CSV_Reader/CSV_Reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSV_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
