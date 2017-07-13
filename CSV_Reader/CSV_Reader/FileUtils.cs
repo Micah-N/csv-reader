3ea9b87483f21d36a2596da1bdc7d1112e0aa003
Micah Netz <mnetz@2ndwatch.com>
2017-07-13 08:58:51 -0700

    Added function to display file contents to console (for debugging)

---
+++ CSV_Reader/CSV_Reader/FileUtils.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSV_Reader
+{
+    public class FileUtils
+    {
+        public FileUtils()
+        {
+        }
+
+        public static string GetReadFile(string[] args)
+        {
+            if (Program.TestForArgs(args))
+            {
+                return args[args.Length - 1];
+            }
+            else
+            {
+                return GetDefaultFileHandle(); //Used for setting up a default test file, remove if not needed
+            }
+        }
+
+        public static string GetDefaultFileHandle()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "test.csv");
+        }
+
+        public static bool TestFileHandle(string target)
+        {
+            return File.Exists(target);
+        }
+
+        public static Dictionary<string, string[]> ReadFile(string target)
+        {
+            StreamReader reader = new StreamReader(new FileStream(target, FileMode.Open));
+            List<string[]> results = new List<string[]>();
+
+            while (!reader.EndOfStream)
+            {
+                results.Add(reader.ReadLine().Split(','));
+            }
+
+            Dictionary<string, string[]> dict = new Dictionary<string, string[]>();
+            string[] keys = results[0];
+            string[,] values = new string[keys.Length, results.Count-1];
+            CheckFileContents(results);
+            /*
+            for (int i = 1; i < results.Count; i++)
+            {
+                for (int j = 0; j < results[i].Length; j++)
+                {
+                    values[j] = results[i][j]; //This needs to be adjusted
+                }
+                dict.Add(keys[i - 1], values[i-1]);
+            }
+            */            
+            return dict;
+        }
+
+        static void CheckFileContents(List<string[]> target)
+        {
+            for (int i = 0; i < target.Count; i++)
+            {
+                Console.WriteLine(target[i]);
+                for (int j = 0; j < target[i].Length; j++)
+                {
+                    Console.WriteLine("Col#" + i + ", value#" + j + ": " + target[i][j]);
+                }
+            }
+        }
+    }
+}
--- CSV_Reader/CSV_Reader/Program.cs
+++ CSV_Reader/CSV_Reader/Program.cs
@@ -10,66 +10,20 @@ namespace CSV_Reader
         {
             Console.WriteLine("Hello World!\n");
 
-            string file = GetReadFile(args);
+            string file = FileUtils.GetReadFile(args);
             
             Console.WriteLine("File: " + file);
-            if (TestFileHandle(file))
+            if (FileUtils.TestFileHandle(file))
             {
-                OutputResults(ReadFile(file));
+                OutputResults(FileUtils.ReadFile(file));
             }
             Console.ReadLine();
-        }
+        }        
 
-        static string GetReadFile(string[] args)
-        {
-            if (TestForArgs(args))
-            {
-                return args[args.Length - 1];
-            }
-            else
-            {
-                return GetDefaultFileHandle(); //Used for setting up a default test file, remove if not needed
-            }
-        }
-
-        static string GetDefaultFileHandle()
-        {            
-            return Path.Combine(Directory.GetCurrentDirectory(), "test.csv");
-        }
-
-        static bool TestForArgs(string[] target)
+        public static bool TestForArgs(string[] target)
         {
             return (target.Length > 0);
-        }
-
-        static bool TestFileHandle(string target)
-        {
-            return File.Exists(target);
-        }
-
-        static Dictionary<string, string[]> ReadFile(string target)
-        {
-            StreamReader reader = new StreamReader(new FileStream(target, FileMode.Open));
-            List<string[]> results = new List<string[]>();
-            
-            while (!reader.EndOfStream)
-            {
-                results.Add(reader.ReadLine().Split(','));
-            }
-
-            Dictionary<string, string[]> dict = new Dictionary<string, string[]>();
-            string[] keys = results[0];
-            string[] temp = new string[results.Count];
-            for(int i = 1; i < results.Count; i++)
-            {
-                for(int j = 0; j < results[i].Length; j++)
-                {
-                    temp[j] = results[i][j]; //This needs to be adjusted
-                }
-                dict.Add(keys[i-1], temp);
-            }
-            return dict;
-        }
+        }        
 
         static void OutputResults(Dictionary<string, string[]> target)
         {
