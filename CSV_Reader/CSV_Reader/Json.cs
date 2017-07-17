2733822a95f979f1d3ea28107540f8dbf30a51c9
Micah Netz <mnetz@2ndwatch.com>
2017-07-17 09:06:49 -0700

    Updates to solution based on feedback from code review: naming conventions, LINQ usage, and multi-line logic

--- CSV_Reader/CSV_Reader/Json.cs
+++ CSV_Reader/CSV_Reader/Json.cs
@@ -1,30 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-namespace CSV_Reader
+namespace CSVReader
 {
     public class Json
     {
         public string[] Keys { get; set; }
         public List<string>[] Values { get; set; }
-
-        override
-        public string ToString()
+        
+        public override string ToString()
         {
             string spaces = "    "; //string holds four spaces for cleaner formatting (looks better than "\t")
-            string results = "{\n  [";
+            string results = $"{{{Environment.NewLine}  [";
             for(int value = 0; value < this.Values[0].Count; value++)
             {
-                results += "\n" + spaces + "{";
+                results += $"{Environment.NewLine}" + spaces + "{";
                 for (int key = 0; key < this.Keys.Length; key++)
                 {
                     string val = this.Values[key][value];
-                    results += "\n" + spaces + spaces + '"' + this.Keys[key].Trim() + '"' + ": " + '"' + val.Trim() + '"';
+                    results += $"{Environment.NewLine} {spaces}{spaces}" + '"' + this.Keys[key].Trim() + '"' + ": " + '"' + val.Trim() + '"';
                     if(key < this.Keys.Length - 1) { results += ","; }
                 }
-                results += "\n" + spaces + "}";              
+                results += $"{Environment.NewLine}{spaces}" + "}";              
                 if(value < this.Values[0].Count - 1) { results += ","; }
             }
-            results += "\n  ]\n}";
+            results += $"{Environment.NewLine}" + $"  ]{ Environment.NewLine}" + "}";
             return results;
         }
     }
